fc0d63735bf5cbe6ccaaf2b97c80c7ccf7943189
ryanvidler14 <ryan.vidler14@gmail.com>
2017-04-13 13:44:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//this section is all the modules we need in application
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+// Initialize appication with route / (that means root of the application)
+app.get('/', function(req, res){
+  var express=require('express');
+  app.use(express.static(path.join(__dirname)));
+  res.sendFile(path.join(__dirname, '../chat-application', 'index.html'));
+});
+
+// Register events on socket connection
+io.on('connection', function(socket){
+  socket.on('chatMessage', function(from, msg){
+    io.emit('chatMessage', from, msg);
+  });
+  socket.on('notifyUser', function(user){
+    io.emit('notifyUser', user);
+  });
+});
+
+// Listen application request on port 8000
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
